f6ceb4cfc5738175c0a1bf47095057bec1956eb1
nikhil <nc13969@gmail.com>
2023-08-29 17:01:49 +0530

    message

--- new.py
+++ new.py
@@ -1,1 +1,16 @@
-"hello"
+import pymongo
+
+
+
+uri = "mongodb+srv://Nikhil09:<Nikhil09>@cluster0.zviugbc.mongodb.net/?retryWrites=true&w=majority"
+client = pymongo.MongoClient(uri)
+
+db = client["mynewdatabase"]
+
+col = db["hnji"]
+
+test = {
+    "name":"nikhil",
+    "age":21
+}
+col.insert_one(test)
