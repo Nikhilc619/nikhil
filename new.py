8b78bf5efde432083f18e06e57786a02d108c507
nikhil <nc13969@gmail.com>
2023-09-01 16:04:22 +0530

    hello

--- new.py
+++ new.py
@@ -1,9 +1,16 @@
 import pymongo
+import json
+import certifi
+
+ca = certifi.where()
+
+
+
 
 
 
 uri = "mongodb+srv://Nikhil09:<Nikhil09>@cluster0.zviugbc.mongodb.net/?retryWrites=true&w=majority"
-client = pymongo.MongoClient(uri)
+client = pymongo.MongoClient(uri, tlsCAFile=ca)
 
 db = client["mynewdatabase"]
 
@@ -14,3 +21,4 @@ test = {
     "age":21
 }
 col.insert_one(test)
+
